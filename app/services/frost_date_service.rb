410ca61e25c1369167923a7989c9cf34125154e4
Joel Grant <joelmgrant@me.com>
2022-03-26 17:10:14 -0600

    Feat: Consume an endpoint that provides frost date information

--- app/services/frost_date_service.rb
+++ app/services/frost_date_service.rb
@@ -4,4 +4,10 @@ class FrostDateService
     response = conn.get("/v1/frostdates/stations/?lat=#{lat}&lon=#{long}")
     JSON.parse(response.body, symbolize_names: true)
   end
+
+  def self.get_spring_frost_dates(weather_station_id)
+    conn = Faraday.new("https://api.farmsense.net")
+    response = conn.get("/v1/frostdates/probabilities/?station=#{weather_station_id}&season=1")
+    JSON.parse(response.body, symbolize_names: true)
+  end
 end
