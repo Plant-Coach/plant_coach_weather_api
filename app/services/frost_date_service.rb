2c4f8e3ec9f52d96420eeca0b8cca660d80f4637
Joel Grant <joelmgrant@me.com>
2022-03-26 17:16:26 -0600

    Feat: Consume api that provides a fall frost date

--- app/services/frost_date_service.rb
+++ app/services/frost_date_service.rb
@@ -10,4 +10,10 @@ class FrostDateService
     response = conn.get("/v1/frostdates/probabilities/?station=#{weather_station_id}&season=1")
     JSON.parse(response.body, symbolize_names: true)
   end
+
+  def self.get_fall_frost_dates(weather_station_id)
+    conn = Faraday.new("https://api.farmsense.net")
+    response = conn.get("/v1/frostdates/probabilities/?station=#{weather_station_id}&season=2")
+    JSON.parse(response.body, symbolize_names: true)
+  end
 end
