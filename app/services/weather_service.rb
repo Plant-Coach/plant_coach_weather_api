73b1ddea953f4cd3ba20c48fea6fb85bf929c68e
Joel Grant <joelmgrant@me.com>
2022-03-27 12:36:33 -0600

    Consume AccuWeather Forecast API

--- app/services/weather_service.rb
+++ app/services/weather_service.rb
@@ -2,9 +2,18 @@ class WeatherService
   def self.location_search(location)
     conn = Faraday.new("http://dataservice.accuweather.com") do |faraday|
       faraday.params["q"] = location
+      faraday.params["details"] = true
       faraday.params["apikey"] = ENV['weather_api_key']
     end
     response = conn.get("/locations/v1/cities/search/")
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def self.get_forecast(location_key)
+    conn = Faraday.new("http://dataservice.accuweather.com") do |faraday|
+      faraday.params["apikey"] = ENV['weather_api_key']
+    end
+    response = conn.get("/forecasts/v1/daily/5day/#{location_key}")
     JSON.parse(response.body, symbolize_names: true)
   end
 end
