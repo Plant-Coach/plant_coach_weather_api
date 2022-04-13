240f42c62c37022e060556acb125f65f0c2bcd8a
Joel Grant <joelmgrant@me.com>
2022-04-13 10:53:01 -0600

    Test: Create basic assertions for a frost date service

--- spec/requests/api/v1/frost_request_spec.rb
+++ spec/requests/api/v1/frost_request_spec.rb
@@ -6,7 +6,13 @@ RSpec.describe '/frost API Endpoints' do
       zip_code = 80123
       get '/api/v1/frost', params: { zip_code: zip_code }
       result = JSON.parse(response.body, symbolize_names: true)
-      require 'pry'; binding.pry
+
+      expect(response).to be_successful
+
+      expect(result).to be_a Hash
+      expect(result).to have_key(:data)
+      expect(result[:data]).to have_key(:type)
+      expect(result[:data][:type]).to eq("frost_date")
     end
   end
 end
