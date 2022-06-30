8b4c7631bb1688e6a38fee5443b897e7771e53d8
Joel Grant <joelmgrant@me.com>
2022-06-30 13:45:07 -0600

    Modify call to create forecast poro

--- app/facades/weather_facade.rb
+++ app/facades/weather_facade.rb
@@ -4,6 +4,6 @@ class WeatherFacade
     latitude = location_results[:lat]
     longitude = location_results[:lon]
     forecast_results = WeatherService.get_forecast(latitude, longitude)
-    forecast_results[:daily].map { |daily_forecast| Forecast.new(daily_forecast) }
+    Forecast.new(forecast_results)
   end
 end
