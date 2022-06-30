83c0b5eb71243160749ca65c33efc0d7374daf4e
Joel Grant <joelmgrant@me.com>
2022-06-30 14:58:15 -0600

    Set up basic alert retrieval tests

---
+++ spec/services/weather_alert_service_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe WeatherAlertService do
+  describe '#check_user_alerts' do
+    it 'requests a list of user zip codes to check if anyone has a weather alert' do
+
+    end
+
+    it 'will check for alerts based on zip code, if any are provided' do
+
+    end
+  end
+end
