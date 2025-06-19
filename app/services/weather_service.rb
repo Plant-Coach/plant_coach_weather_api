ccafdf2d8b64db146457557b830dbfc5e4fbe785
Joel Grant <69736499+joel-grant@users.noreply.github.com>
2025-06-19 16:08:24 -0600

    Add external weather service's API key (#27)
    * Update API Key for weather service'
    
    * update branch condition for testing
    
    * fix formatting
    
    * try to fix parsing error
    
    * branch conditions again
    
    * revert to test
    
    * try new flag again
    
    * revert branch conditions

--- app/services/weather_service.rb
+++ app/services/weather_service.rb
@@ -1,12 +1,12 @@
 class WeatherService
   def self.location_search(location)
-    response = Faraday.get("http://api.openweathermap.org/geo/1.0/zip?zip=#{location}&appid=#{ENV['weather_api_key']}")
+    response = Faraday.get("http://api.openweathermap.org/geo/1.0/zip?zip=#{location}&appid=#{ENV['OPENWEATHER_API_KEY']}")
     JSON.parse(response.body, symbolize_names: true)
   end
 
   def self.get_forecast(lat, lon)
     conn = Faraday.new("https://api.openweathermap.org") do |faraday|
-      faraday.params["appid"] = ENV['weather_api_key']
+      faraday.params["appid"] = ENV['OPENWEATHER_API_KEY']
       faraday.params["lat"] = lat
       faraday.params["lon"] = lon
       faraday.params[:exclude] = "minutely,hourly"
