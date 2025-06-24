083303e7c1fd4eabd37c4110828351774b92cee1
Joel Grant <joelmgrant@me.com>
2025-06-24 14:31:26 -0600

    feat: add kubernetes health probe setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get "/rails/health" => "rails/health#show", as: :rails_health_check
+  get "/rails/ready" => "rails/ready#show", as: :rails_ready_check
   namespace :api do
     namespace :v1 do
       resources :forecast, only: [:create]
