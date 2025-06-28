f5827d23618cef2efa926a76892b2dcef83b5389
Joel Grant <69736499+joel-grant@users.noreply.github.com>
2025-06-28 14:08:15 -0600

    fix location of response

--- app/services/frost_date_service.rb
+++ app/services/frost_date_service.rb
@@ -2,13 +2,13 @@ class FrostDateService
   def self.find_weather_station(lat, long)
     begin
       conn = Faraday.new('https://api.farmsense.net')
+      response = conn.get("/v1/frostdates/stations/?lat=#{lat}&lon=#{long}")
       if response.status == 200
         JSON.parse(response.body, symbolize_names: true)
       else
         Rails.logger.error("API request failed with status code: #{response.status}")
         nil # Or return a structured error object
       end
-      response = conn.get("/v1/frostdates/stations/?lat=#{lat}&lon=#{long}")
       JSON.parse(response.body, symbolize_names: true)
     rescue Faraday::TimeoutError, Faraday::ConnectionFailed => e
       Rails.logger.error("Network error: #{e.message}")
@@ -22,13 +22,13 @@ class FrostDateService
   def self.get_spring_frost_dates(weather_station_id)
     begin
       conn = Faraday.new('https://api.farmsense.net')
+      response = conn.get("/v1/frostdates/probabilities/?station=#{weather_station_id}&season=1")
       if response.status == 200
         JSON.parse(response.body, symbolize_names: true)
       else
         Rails.logger.error("API request failed with status code: #{response.status}")
         nil # Or return a structured error object
       end
-      response = conn.get("/v1/frostdates/probabilities/?station=#{weather_station_id}&season=1")
       JSON.parse(response.body, symbolize_names: true)
     rescue Faraday::TimeoutError, Faraday::ConnectionFailed => e
       Rails.logger.error("Network error: #{e.message}")
@@ -42,13 +42,13 @@ class FrostDateService
   def self.get_fall_frost_dates(weather_station_id)
     begin
       conn = Faraday.new('https://api.farmsense.net')
+      response = conn.get("/v1/frostdates/probabilities/?station=#{weather_station_id}&season=2")
       if response.status == 200
         JSON.parse(response.body, symbolize_names: true)
       else
         Rails.logger.error("API request failed with status code: #{response.status}")
         nil # Or return a structured error object
       end
-      response = conn.get("/v1/frostdates/probabilities/?station=#{weather_station_id}&season=2")
       JSON.parse(response.body, symbolize_names: true)
     rescue Faraday::TimeoutError, Faraday::ConnectionFailed => e
       Rails.logger.error("Network error: #{e.message}")
