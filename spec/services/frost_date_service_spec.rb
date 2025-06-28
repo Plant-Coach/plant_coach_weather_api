66268a970b214518a69817f6f0b0a420d4270797
Joel Grant <69736499+joel-grant@users.noreply.github.com>
2025-06-28 14:22:52 -0600

    test: connection failed error

--- spec/services/frost_date_service_spec.rb
+++ spec/services/frost_date_service_spec.rb
@@ -6,19 +6,7 @@ RSpec.describe FrostDateService do
   describe '::find_weather_station' do
     let(:latitude) { 40.7128 }
     let(:longitude) { -74.0060 }
-    
-    context 'when a network error occurs' do
-      it 'returns nil and logs the error' do
-        allow(Faraday).to receive(:new).and_raise(Faraday::TimeoutError.new('Timeout'))
-        allow(Rails.logger).to receive(:error)
-
-        result = FrostDateService.find_weather_station(latitude, longitude)
-
-        expect(result).to be_nil
-        
-        expect(Rails.logger).to have_received(:error).with(/Network error/)
-      end
-    end
+     
     it 'retrieves the closest weather station to the coordinates' do
       data = FrostDateService.find_weather_station(39.7392, -104.9903)
       expect(data).to be_an Array
@@ -35,6 +23,33 @@ RSpec.describe FrostDateService do
       expect(data[0]).to have_key(:lon)
       expect(data[0][:lon]).to be_a String
     end
+
+    context 'when a timeout error occurs' do
+      it 'returns nil and logs the error' do
+        allow(Faraday).to receive(:new).and_raise(Faraday::TimeoutError.new('Timeout'))
+        allow(Rails.logger).to receive(:error)
+
+        result = FrostDateService.find_weather_station(latitude, longitude)
+
+        expect(result).to be_nil
+        
+        expect(Rails.logger).to have_received(:error).with(/Network error/)
+      end
+    end
+    
+    context 'when a connectionfailed error occurs' do
+      it 'returns nil and logs the error' do
+        allow(Faraday).to receive(:new).and_raise(Faraday::ConnectionFailed.new('ConnectionFailed'))
+        allow(Rails.logger).to receive(:error)
+
+        result = FrostDateService.find_weather_station(latitude, longitude)
+
+        expect(result).to be_nil
+        
+        expect(Rails.logger).to have_received(:error).with(/Network error/)
+      end
+    end
+   
   end
 
   describe '::get_spring_frost_dates' do
