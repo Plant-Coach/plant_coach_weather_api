b9c58905441ecce3ae8e791e6f790be95bc41e99
Joel Grant <69736499+joel-grant@users.noreply.github.com>
2025-06-28 13:10:14 -0600

    Fix: handle exceptions due to external APIs

--- app/services/frost_date_service.rb
+++ app/services/frost_date_service.rb
@@ -1,19 +1,43 @@
 class FrostDateService
   def self.find_weather_station(lat, long)
-    conn = Faraday.new('https://api.farmsense.net')
-    response = conn.get("/v1/frostdates/stations/?lat=#{lat}&lon=#{long}")
-    JSON.parse(response.body, symbolize_names: true)
+    begin
+      conn = Faraday.new('https://api.farmsense.net')
+      response = conn.get("/v1/frostdates/stations/?lat=#{lat}&lon=#{long}")
+      JSON.parse(response.body, symbolize_names: true)
+    rescue Faraday::TimeoutError, Faraday::ConnectionFailed => e
+      Rails.logger.error("Network error: #{e.message}")
+      nil
+    rescue JSON::ParserError => e
+      Rails.logger.error("Failed to parse JSON response: #{e.message}")
+      nil
+    end
   end
 
   def self.get_spring_frost_dates(weather_station_id)
-    conn = Faraday.new('https://api.farmsense.net')
-    response = conn.get("/v1/frostdates/probabilities/?station=#{weather_station_id}&season=1")
-    JSON.parse(response.body, symbolize_names: true)
+    begin
+      conn = Faraday.new('https://api.farmsense.net')
+      response = conn.get("/v1/frostdates/probabilities/?station=#{weather_station_id}&season=1")
+      JSON.parse(response.body, symbolize_names: true)
+    rescue Faraday::TimeoutError, Faraday::ConnectionFailed => e
+      Rails.logger.error("Network error: #{e.message}")
+      nil
+    rescue JSON::ParserError => e
+      Rails.logger.error("Failed to parse JSON response: #{e.message}")
+      nil
+    end
   end
 
   def self.get_fall_frost_dates(weather_station_id)
-    conn = Faraday.new('https://api.farmsense.net')
-    response = conn.get("/v1/frostdates/probabilities/?station=#{weather_station_id}&season=2")
-    JSON.parse(response.body, symbolize_names: true)
+    begin
+      conn = Faraday.new('https://api.farmsense.net')
+      response = conn.get("/v1/frostdates/probabilities/?station=#{weather_station_id}&season=2")
+      JSON.parse(response.body, symbolize_names: true)
+    rescue Faraday::TimeoutError, Faraday::ConnectionFailed => e
+      Rails.logger.error("Network error: #{e.message}")
+      nil
+    rescue JSON::ParserError => e
+      Rails.logger.error("Failed to parse JSON response: #{e.message}")
+      nil
+    end
   end
 end
